1be1911d7a8175d86d9f39c112f7c60b3b26eef0
benjaminjsteenhoek@gmail.com <benjaminjsteenhoek@gmail.com>
2021-07-17 16:32:25 -0500

    Fix locations

--- projects/extensions/jpanlib/src/main/java/databaseNodes/ASTDatabaseNode.java
+++ projects/extensions/jpanlib/src/main/java/databaseNodes/ASTDatabaseNode.java
@@ -9,6 +9,7 @@ import ast.ASTNode;
 import ast.CodeLocation;
 import ast.expressions.Expression;
 import ast.expressions.Callee;
+import ast.functionDef.FunctionDefBase;
 import ast.functionDef.ParameterBase;
 import ast.functionDef.ParameterList;
 import ast.functionDef.ReturnType;
@@ -39,22 +40,20 @@ public class ASTDatabaseNode extends DatabaseNode
 		// if(astNode.getChildCount() == 0)
 		properties.put(NodeKeys.CODE, astNode.getEscapedCodeStr());
 		
-		// if (astNode instanceof Identifier) {
-		// 	properties.put(NodeKeys.LOCATION, getCorrectedLocationString());
-			// properties.put(NodeKeys.LOCATION, astNode.getLocationString());
-		// }
-		properties.put(NodeKeys.LOCATION, getCorrectedLocationString());
+		// Some nodes already have the correct location
+		if (nodeNeedsCorrection())
+		{
+			properties.put(NodeKeys.LOCATION, getCorrectedLocationString());
+		}
+		else
+		{
+			properties.put(NodeKeys.LOCATION, astNode.getLocation().toString());
+		}
 
 
 		if (astNode.isInCFG())
 		{
 			properties.put(NodeKeys.IS_CFG_NODE, "True");
-			properties.put(NodeKeys.LOCATION, getCorrectedLocationString());
-		}
-
-		if (astNode instanceof CompoundStatement)
-		{
-			properties.put(NodeKeys.LOCATION, astNode.getLocationString());
 		}
 
 		if (astNode instanceof Expression)
@@ -72,11 +71,11 @@ public class ASTDatabaseNode extends DatabaseNode
 		return properties;
 	}
 
-	private boolean nodeIsInParameterList()
+	private boolean nodeNeedsCorrection()
 	{
 		// Early return for known type
-		if (astNode instanceof ParameterList) {
-			return true;
+		if ((astNode instanceof ParameterList) || (astNode instanceof ParameterBase) || (astNode instanceof Callee) || (astNode instanceof ReturnType)) {
+			return false;
 		}
 
 		// BFS with predecessors
@@ -86,17 +85,26 @@ public class ASTDatabaseNode extends DatabaseNode
 		while (!queue.isEmpty()) {
 			ASTNode u = queue.remove();
 			if (u == astNode) {
-				// Trace back path to function AST root.
-				// Return whether an ancestor is ParameterList.
-				ASTNode pointer = u;
-				while (pred.containsKey(pointer)) {
-					ASTNode next = pred.get(pointer);
-					pointer = next;
-					if (pointer instanceof ParameterList) {
-						return true;
+				if (u instanceof CompoundStatement) {
+					if (pred.get(u) instanceof FunctionDefBase) {
+						return false;
 					}
+					return true;
 				}
-				return false;
+				else
+				{
+					// Trace back path to function AST root.
+					// Return whether an ancestor is ParameterList.
+					ASTNode pointer = u;
+					while (pred.containsKey(pointer)) {
+						ASTNode next = pred.get(pointer);
+						pointer = next;
+						if (pointer instanceof ParameterList) {
+							return false;
+						}
+					}
+					return true;
+				}
 			}
 			// Add all children to queue and log predecessors
 			for (int i = 0; i < u.getChildCount(); i ++) {
@@ -105,34 +113,23 @@ public class ASTDatabaseNode extends DatabaseNode
 				queue.add(v);
 			}
 		}
-		throw new RuntimeException(String.format("Error: didn't find ASTNode %s in FunctionDatabaseNode %s.", astNode.toString(), currentFunction.toString()));
+		throw new RuntimeException(String.format("Error: didn't find ASTNode %s (%s) in FunctionDatabaseNode %s.", astNode.toString(), astNode.getTypeAsString(), currentFunction.toString()));
 	}
 
 	private String getCorrectedLocationString()
 	{
-		// Old brittle condition
-		// if (!(astNode instanceof ParameterBase) || (astNode instanceof ReturnType))
+		CodeLocation funcLocation = currentFunction.getContentLocation();
+		CodeLocation tempLocation = new CodeLocation();
+		tempLocation.startLine = astNode.getLocation().startLine;
+		tempLocation.endLine = astNode.getLocation().endLine;
+		tempLocation.startPos = astNode.getLocation().startPos;
+		tempLocation.startIndex = astNode.getLocation().startIndex;
+		tempLocation.stopIndex = astNode.getLocation().stopIndex;
 
-		// If astNode's ancestor is ParameterList, don't modify its location
-		if (!nodeIsInParameterList() && !(astNode instanceof Callee))
-		{
-			CodeLocation funcLocation = currentFunction.getContentLocation();
-			CodeLocation tempLocation = new CodeLocation();
-			tempLocation.startLine = astNode.getLocation().startLine;
-			tempLocation.endLine = astNode.getLocation().endLine;
-			tempLocation.startPos = astNode.getLocation().startPos;
-			tempLocation.startIndex = astNode.getLocation().startIndex;
-			tempLocation.stopIndex = astNode.getLocation().stopIndex;
-
-			tempLocation.startIndex += funcLocation.startIndex + 1;
-			tempLocation.startLine += funcLocation.startLine - 1;
-			tempLocation.stopIndex += funcLocation.startIndex + 1;
-			return tempLocation.toString();
-		}
-		else
-		{
-			return astNode.getLocation().toString();
-		}
+		tempLocation.startIndex += funcLocation.startIndex + 1;
+		tempLocation.startLine += funcLocation.startLine - 1;
+		tempLocation.stopIndex += funcLocation.startIndex + 1;
+		return tempLocation.toString();
 	}
 
 	public FunctionDatabaseNode getCurrentFunction()
